07bd89c39895ba40ab610eb36406a67dfe43d540
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-24 18:59:37 -0700

    Added hooks folder

---
+++ src/hooks/useFlip.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const useFlip = (initialValue = true) => {
+    const [isFlipped, setIsFlipped] = useState(initialValue);
+
+    const flip = () => {
+        setIsFlipped
+
+    }
+
+}
+
+export default useFlip;
